e17da97be2264005c7db59b7d3edfb1a17bfb934
Raunak Chowdhuri <raunakc@mit.edu>
2023-12-27 13:25:47 -0800

    updated script

--- app.py
+++ app.py
@@ -8,13 +8,30 @@ import streamlit as st
 "# Degirum Test Plan Parser"
 
 # Uploading the HTML file
-uploaded_file = st.file_uploader("Choose a HTML file", type="html")
-if uploaded_file is None:
+uploaded_files = st.file_uploader(
+    "Choose a HTML file", type="html", accept_multiple_files=True
+)
+
+if uploaded_files is None or len(uploaded_files) == 0:
     st.stop()
 
+title_headings = []
+for uploaded_file in uploaded_files:
+    soup = BeautifulSoup(uploaded_file.read().decode(), "html.parser")
+    title_headings.append(soup.find(id="title-heading").text)
+    uploaded_file.seek(0)
+
+selection = st.selectbox(
+    "Select Test Plan", title_headings, index=None, placeholder="Select a test plan"
+)
+
+if selection is None:
+    st.stop()
+
+uploaded_file = uploaded_files[title_headings.index(selection)]
+
+
 html_content = uploaded_file.read().decode()
-
-
 # Parsing the HTML content using BeautifulSoup
 soup = BeautifulSoup(html_content, "html.parser")
 
@@ -113,4 +130,12 @@ b64 = base64.b64encode(
 href = (
     f'<a href="data:file/csv;base64,{b64}" download="output.csv">Download CSV File</a>'
 )
+st.markdown(href, unsafe_allow_html=True)
+
+
+df.to_excel("output.xlsx", index=False)
+with open("output.xlsx", "rb") as f:
+    data = f.read()
+b64 = base64.b64encode(data).decode()
+href = f'<a href="data:application/octet-stream;base64,{b64}" download="output.xlsx">Download Excel File</a>'
 st.markdown(href, unsafe_allow_html=True)
